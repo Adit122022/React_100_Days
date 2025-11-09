833e8792f8a9fbc7dc33d4fc92f0f3690ecaf688
Adit122022 <adity202212@gmail.com>
2025-11-09 10:28:21 +0530

    create and delete is done now i am changing the id

---
+++ 10_Expence_Tracker/src/zustand/useExpense.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand'
+import {persist} from 'zustand/middleware'
+
+export const useExpense = create(persist(
+    (set) => ({
+    expenses:[],
+    setExpenses:(payload)=>set((state)=>({ expenses:[...state.expenses,{ id: Date.now(), ...payload }]})) ,
+    deleteExpenses:(deleteid)=>set((state)=>({expenses:state.expenses.filter((items, id)=>id!==deleteid)}))
+}),{name:'expenses'} ))
