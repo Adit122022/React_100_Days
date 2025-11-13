f6614e34620b412f8639cd357c012959e7b6489c
Adit122022 <adity202212@gmail.com>
2025-11-13 23:22:59 +0530

    starting something new

---
+++ E-com/src/App.tsx
@@ -0,0 +1,11 @@
+import Admin from "./components/Admin/Admin"
+
+const App = () => {
+  return (
+    <>
+      <Admin />
+    </>
+  )
+}
+
+export default App
---
+++ E-com/src/components/Admin/Admin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='bg-[#F8F7F4] h-screen flex items-center'>
+      
+    </div>
+  )
+}
+
+export default Admin
