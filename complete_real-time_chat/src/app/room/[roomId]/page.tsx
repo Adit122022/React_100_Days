f320063fe7e63223f9c140fc4751426b04984cf1
Adit122022 <adity202212@gmail.com>
2026-01-30 00:33:20 +0530

    proxy is a kind of middleware in next js  where  //  OVERVIEW : CHECK IF USER IS ALLOWED TO JOIN ROOM //  IF THEY ARE : LET THEM PASS //  IF THEY ARE NOT : SEND THEM TO LOBBY  this is the work to be done
      TODO : TOKEN FROM REDIS

--- complete_real-time_chat/src/app/room/[roomId]/page.tsx
+++ complete_real-time_chat/src/app/room/[roomId]/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Page = () => {
     const [copyStatus, setCopyStatus] = useState("COPY")
+    const [input, setInput] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null);
     const [timeRemaining, setTimeRemainimg] = useState<number | null>(null)
     const params = useParams()
     const roomId = params.roomId as string;
@@ -62,10 +64,17 @@ const Page = () => {
                         <span className='absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse-'>
                             {">"}
                         </span>
-                        <input autoFocus className='w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm' type="text" name="" id="" />
+                        <input ref={inputRef} onChange={(e) => setInput(e.target.value)} value={input} onKeyDown={(e) => {
+                            if (e.key === "Enter" && input.trim()) {
+                                // TODO :SEND MESSAGE 
+                                inputRef.current?.focus()
+                            }
+                        }}
+                            placeholder='Type Message ...'
+                            autoFocus className='w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm' type="text" name="" id="" />
                     </div>
 
-                    <button className='bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50'>SEND</button>
+                    <button className='bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer'>SEND</button>
                 </div>
             </div>
         </main>
---
+++ complete_real-time_chat/src/proxy.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server"
+import { redis } from "./lib/redis";
+
+export const proxy = async (req:NextRequest )=>{
+const pathname = req.nextUrl.pathname;
+ const roomMatch = pathname.match(/^\/room\/([^/]+)$/)
+
+ if(!roomMatch) return  NextResponse.redirect(new URL("/", req.url))
+
+const roomId = roomMatch[1]
+const meta = await redis.hgetall<{connected:string[], createdAt :number}>(`meta:${roomId}`)
+
+ if(!meta) return  NextResponse.redirect(new URL("/?error=room-not-found", req.url))
+
+    // TODO : TOKEN arbatery tokens from REDIS DATABASES 
+
+//  OVERVIEW : CHECK IF USER IS ALLOWED TO JOIN ROOM
+//  IF THEY ARE : LET THEM PASS
+//  IF THEY ARE NOT : SEND THEM TO LOBBY
+}
+
+
+export const config ={
+    matcher: "/room/:path*",
+}
