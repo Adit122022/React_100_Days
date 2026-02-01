e34ab0fe687699fd2791c25456ad065317dad109
Adit122022 <adity202212@gmail.com>
2026-02-01 11:29:33 +0530

    initalizinf a next js project that could be mac-portfolio

---
+++ 06_mac-portfolio/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+const page = () => {
+  return (
+    <main
+      style={{ backgroundImage: 'url("./mac-wallpaper.jpg")' }}
+      className="w-screen h-screen bg-center bg-conver bg-no-repeat"
+    >
+      {/* Content goes here */}
+    </main>
+  )
+}
+
+export default page
